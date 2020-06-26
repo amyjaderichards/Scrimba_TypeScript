7c1f710d2dbe591ace1b1abf624620c8532de867
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-26 22:35:25 +0100

    Exercise 4 completed

---
+++ 04-Arrays/index.ts
@@ -0,0 +1,10 @@
+const example1: string[] = ['Hello'];
+
+const example2: number[] = [1, 2];
+
+const example3: boolean[] = [true, false];
+
+const example4: (number | boolean)[] = [true, false, 9];
+
+//nested arrays
+const example5: boolean[][] = [ [true, false] ];

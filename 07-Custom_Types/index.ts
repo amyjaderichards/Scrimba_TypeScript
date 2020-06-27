12f4ffe593582167231be294972a57656d9699c7
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-27 12:01:46 +0100

    Exercise 7 completed

---
+++ 07-Custom_Types/index.ts
@@ -0,0 +1,9 @@
+type person = {firstName: string}
+
+const example1: object = undefined;
+
+const example2: Object = NaN;
+
+const example3: person = {firstName: 'Amie'};
+
+example3.firstName = 'Amy'

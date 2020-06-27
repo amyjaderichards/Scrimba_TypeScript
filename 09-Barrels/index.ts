eb7fd016b8ca5f754b5f08a514fb416b399ef666
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-27 12:13:56 +0100

    Exercise 9 completed

---
+++ 09-Barrels/index.ts
@@ -0,0 +1,8 @@
+import { IBear } from './interfaces/bear.interface';
+import { IMan } from './interfaces/man.interface';
+import { IPig } from './interfaces/pig.interface';
+
+// barrel
+import { IBear, IMan, IPig } from './interfaces';
+
+

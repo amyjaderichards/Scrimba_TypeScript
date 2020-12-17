e3b9f2ba0100373b9652e24f0e3fd7c9a727fca4
Amy <amyjaderichards@googlemail.com>
2020-12-17 21:13:04 +0000

    Exercise 3 completed

--- 03-Type_Assertions/index.ts
+++ 03-Type_Assertions/index.ts
@@ -1,9 +1,5 @@
-let definitelyNotAString = 'I am a string';
+// As this is type 'any' we need to cast it as a string before 
+// using the string properties
+let definitelyNotAString: any = 'I am a string';
 
-let strLength = definitelyNotAString.length;
-
-// method 1
-let strLength = (definitelyNotAString as string).length;
-
-//method 2
 let strLength = (<string> definitelyNotAString).length;
